e75aa6d2d2680e413457b558e3152f9a27f26beb
readmecode <aasemenin@gmail.com>
2023-07-22 20:53:39 +0300

    update

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+import { Provider } from "react-redux";
+import { store } from "./services/store";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+);
