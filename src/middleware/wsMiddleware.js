d3d5342481a1b06bfe0347e171cf1a9c89577218
readmecode <aasemenin@gmail.com>
2023-07-29 15:40:16 +0300

    upd

--- src/middleware/wsMiddleware.js
+++ src/middleware/wsMiddleware.js
@@ -1,57 +1,51 @@
-export const socketMiddleware = (wsActions) => {
-  return (store) => {
-    let socket = null;
+export const socketMiddleware = wsActions => {
+  return store => {
+  let socket = null;
 
-    return (next) => (action) => {
-      const { dispatch } = store;
-      const { type, payload } = action;
-      const { wsInit, onOpen, onClose, onError, onMessage } = wsActions;
+  return next => action => {
+    const { dispatch } = store;
+    const { type, payload } = action;
+    const { wsInit, onOpen, onClose, onError, onMessage } = wsActions
 
-      if (type === wsInit) {
-        socket = new WebSocket(payload);
-      }
-
-      if (type === wsInit && socket?.readyState === 1) {
+    if (type === wsInit) {
+      if (socket?.readyState === 1) {
         socket.close();
       }
+      socket = new WebSocket(payload);
+    }
 
-      if (socket) {
-        socket.onopen = (e) => {
-          console.log(e);
-          dispatch({ type: onOpen });
-        };
+    if (socket) {
+      socket.onopen = (e) => {
+        console.log(e)
+        dispatch({ type: onOpen });
+      };
 
-        socket.onerror = (e) => {
-          console.log(e);
-          dispatch({ type: onError });
-        };
+      socket.onerror = (e) => {
+        console.log(e)
+        dispatch({ type: onError });
+      };
+      
+      socket.onmessage = (event) => {
+        const { data } = event;
+        const parsedData = JSON.parse(data);
+        const { ...restParsedData } = parsedData;
 
-        socket.onmessage = (event) => {
-          const { data } = event;
-          const parsedData = JSON.parse(data);
-          const { ...restParsedData } = parsedData;
+        if (restParsedData.orders) {
+          restParsedData.orders.sort(
+            (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+          );
 
-          if (restParsedData.orders) {
-            restParsedData.orders.sort(
-              (a, b) =>
-                new Date(b.createdAt).getTime() -
-                new Date(a.createdAt).getTime()
-            );
+          dispatch({ type: onMessage, payload: restParsedData });
+        }
+      };
 
-            dispatch({ type: onMessage, payload: restParsedData });
-          }
-        };
-        socket.onclose = (event) => {
-          dispatch({ type: "WS_CONNECTION_CLOSED", payload: event });
-        };
+      socket.onclose = event => {
+        console.log(event)
+        dispatch({ type: onClose, payload: event });
+      };
+    }
 
-        socket.onclose = (e) => {
-          console.log(e);
-          dispatch({ type: onClose });
-        };
-      }
-
-      next(action);
-    };
+    next(action);
+  };
   };
 };
--- src/services/store.js
+++ src/services/store.js
@@ -33,3 +33,4 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(socketMiddleware(wsActions)),
 });
+
